c7e990b6c1c89db2b1b640e8ddcf3bececa1256f
Camille MacLaren <camillemaclaren@gmail.com>
2023-05-28 18:47:11 -0500

    added arrays and booleans
    commented out starter code temporarily
    added arrays of possible char for password
    added boolean logic for the pool selection of char included

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-
+/*  //starter code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +15,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//*/
+//////////////////////////////////////////////////////////////
+
+
+//init arrays -------------------------
+var pool = Array();
+
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = Array();
+for( let i = 0; i < lower.length; i++)
+{
+  upper[i] = lower[i].toUpperCase();
+}
+var numeric = ["0","1","2","3","4","5","6","7","8","9"];
+
+//the special array: " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+// " = \u0022, ' = \u0027, \ = \u005c
+var special = [" ", "!", "\u0022", "#", "$", "%", "&", "\u0027", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
+              "<", "=", ">", "?", "@", "[", "\u005c", "]", "^", "_", "`", "{", "|", "~"];
+
+
+//establish booleans
+var lowerSelected = true;
+var upperSelected = false;
+var numericSelected = true;
+var specialSelected = false;
+
+if(lowerSelected)   {  pool = pool.concat(lower); }
+if(upperSelected)   {  pool = pool.concat(upper); }
+if(numericSelected) {  pool = pool.concat(numeric); }
+if(specialSelected) {  pool = pool.concat(special); }
+console.log(pool);
