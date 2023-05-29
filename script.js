dd3ff3634d993fda773bedf2f2f63a2b40e2fc8c
Camille MacLaren <camillemaclaren@gmail.com>
2023-05-28 21:52:52 -0500

    put code into functions
    made prev code into the generate password
    put initializers outside said function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Assignment code here
 
-/*  //starter code
+//*  //starter code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,11 +14,12 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 //*/
-//////////////////////////////////////////////////////////////
 
+//Init||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 //init arrays ------------------------------------------
 var pool = Array();
@@ -33,75 +35,80 @@ var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 //the special array: " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 // " = \u0022, ' = \u0027, \ = \u005c
 var special = [" ", "!", "\u0022", "#", "$", "%", "&", "\u0027", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
-              "<", "=", ">", "?", "@", "[", "\u005c", "]", "^", "_", "`", "{", "|", "~"];
-
+                "<", "=", ">", "?", "@", "[", "\u005c", "]", "^", "_", "`", "{", "|", "~"];
 
 //establish booleans ------------------------------------
 var lowerSelected = true;
-var upperSelected = true;
-var numericSelected = false;
-var specialSelected = true;
-
-//add array to pool if selected
-if(lowerSelected)   {  pool = pool.concat(lower); }
-if(upperSelected)   {  pool = pool.concat(upper); }
-if(numericSelected) {  pool = pool.concat(numeric); }
-if(specialSelected) {  pool = pool.concat(special); }
-
-
-//generate password -------------------------------------------------
-
+var upperSelected = false;
+var numericSelected = true;
+var specialSelected = false;
+// length of password
 var numOfChar = 8;
-var word = Array();
-//adding a new char randomly chosen from pool for each numOFChar
-for (let i = 0; i < numOfChar; i++)
+
+//end of Init||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+function generatePassword()
 {
-  word[i] = pool[Math.floor(Math.random()* pool.length)];
-}
-console.log(numOfChar);
-console.log(word);
+  //add array to pool if selected
+  if(lowerSelected)   {  pool = pool.concat(lower); }
+  if(upperSelected)   {  pool = pool.concat(upper); }
+  if(numericSelected) {  pool = pool.concat(numeric); }
+  if(specialSelected) {  pool = pool.concat(special); }
 
+  //generate password -------------------------------------------------
 
-// guarantee the selected types charcater is in word
-//makes an array of 4 random indexes to potentially replace
-var gar = Array();
-var curr;
-for (let i = 0; i < 4; i++)
-{
-  //makes random index
-  curr = Math.floor(Math.random()* numOfChar);
-
-  if(!gar.includes(curr)) //if there are no repeats add new number
+  var word = Array();
+  //adding a new char randomly chosen from pool for each numOFChar
+  for (let i = 0; i < numOfChar; i++)
   {
-    gar[i] = curr;
+    word[i] = pool[Math.floor(Math.random()* pool.length)];
   }
-  else //if there are repeats
-  {
-    while(gar.includes(curr)) //loops until number is no longer a repeat
-    {
-      curr = Math.floor(Math.random()* numOfChar);
-    }
-    gar[i] = curr;
-  }
+
+  // guarantee the selected types charcater is in word
+  //makes an array of 4 random indexes to potentially replace
+  var gar = randomIndex(0);
+  console.log(gar);
+
+  //if selected replaces char using randomly generated index form gar
+  if(lowerSelected)   {  word[gar[0]] = lower[Math.floor(Math.random()* lower.length)] }
+  if(upperSelected)   {  word[gar[1]] = upper[Math.floor(Math.random()* upper.length)] }
+  if(numericSelected) {  word[gar[2]] = numeric[Math.floor(Math.random()* numeric.length)] }
+  if(specialSelected) {  word[gar[3]] = special[Math.floor(Math.random()* special.length)] }
+  console.log(word);
+
+  //convert word to string
+  //join combines the char with seperator in parameter, and trim removes white spaces
   
+  return word.join('').trim();
+
 }
-console.log(gar);
-
-//if selected replaces char using randomly generated index form gar
-if(lowerSelected)   {  word[gar[0]] = lower[Math.floor(Math.random()* lower.length)] }
-if(upperSelected)   {  word[gar[1]] = upper[Math.floor(Math.random()* upper.length)] }
-if(numericSelected) {  word[gar[2]] = numeric[Math.floor(Math.random()* numeric.length)] }
-if(specialSelected) {  word[gar[3]] = special[Math.floor(Math.random()* special.length)] }
-console.log(word);
-
-//convert word to string
-//join combines the char with seperator in parameter, and trim removes white spaces
-var newWord = word.join('').trim();
-console.log(newWord);
 
 
+function randomIndex()
+{
+  var gar = Array();
+  var curr;
+  for (let i = 0; i < 4; i++)
+  {
+    //makes random index
+    curr = Math.floor(Math.random()* numOfChar);
 
-
+    if(!gar.includes(curr)) //if there are no repeats add new number
+    {
+      gar[i] = curr;
+    }
+    else //if there are repeats
+    {
+      while(gar.includes(curr)) //loops until number is no longer a repeat
+      {
+        curr = Math.floor(Math.random()* numOfChar);
+      }
+      gar[i] = curr;
+    }
+    
+  }
+  return gar;
+}
 
 
 
