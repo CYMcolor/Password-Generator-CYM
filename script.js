a21ebcf9c6baff4f4ae2d50e289d26c60aaef672
Camille MacLaren <camillemaclaren@gmail.com>
2023-05-30 19:39:16 -0500

    added left out characters

--- script.js
+++ script.js
@@ -17,11 +17,9 @@ function writePassword() {
 var getNum = function() 
 {
   var inputLength = document.getElementById("charLength").value;
- 
-  console.log(inputLength);
   return inputLength;
 };
-
+//check if unmber is in the range of 8-128
 var checkNum = function(number)
 {
   if(number < 8 || number > 128)
@@ -41,7 +39,7 @@ generateBtn.addEventListener("click", writePassword);
 //init arrays ------------------------------------------
 var pool = Array();
 
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = Array();
 for( let i = 0; i < lower.length; i++)
 {
@@ -52,7 +50,7 @@ var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 //the special array: " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 // " = \u0022, ' = \u0027, \ = \u005c
 var special = [" ", "!", "\u0022", "#", "$", "%", "&", "\u0027", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
-                "<", "=", ">", "?", "@", "[", "\u005c", "]", "^", "_", "`", "{", "|", "~"];
+                "<", "=", ">", "?", "@", "[", "\u005c", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 //establish booleans ------------------------------------
 var lowerSelected = false;
@@ -108,14 +106,15 @@ function generatePassword()
   numOfChar = getNum();
   //checks validity and if wrong returns message
   if(!checkNum(numOfChar))
-   {return "Warning Character Length is Invalid!"; } 
-  
-   // creates the pool of chacters
-   if(!booleanChecks())
-   {
-    return "At Least one character type must be checked!"
-   }
-  console.log(booleanChecks());
+  {
+    return "Warning Character Length is Invalid!"; 
+  } 
+   // assign boolean variables
+  if(!booleanChecks())
+  {
+    return "At least one character type must be checked!"
+  }
+  //creates pool of characters
   generatePool();
 
   var word = Array();
@@ -129,7 +128,7 @@ function generatePassword()
   //makes an array of 4 random indexes to potentially replace
   var gar = randomIndex();
 
-  //if selected replaces char using randomly generated index form gar
+  //if selected replaces char using randomly generated indexes with selected charcter type 
   if(lowerSelected)   {  word[gar[0]] = lower[Math.floor(Math.random()* lower.length)] }
   if(upperSelected)   {  word[gar[1]] = upper[Math.floor(Math.random()* upper.length)] }
   if(numericSelected) {  word[gar[2]] = numeric[Math.floor(Math.random()* numeric.length)] }
@@ -164,6 +163,7 @@ function randomIndex()
       gar[i] = curr;
     }
   }
+  console.log("lower, upper, numeric, special");
   console.log(gar);
 
   return gar;
